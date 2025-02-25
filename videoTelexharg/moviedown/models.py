ae4d518b5e0cce5d4096ab4709d0fb5943dd2968
Brayan dev <143706179+Brayantematio@users.noreply.github.com>
2025-02-25 16:13:12 +0100

    Update models.py

--- videoTelexharg/moviedown/models.py
+++ videoTelexharg/moviedown/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Video(models.Model):
+    title = models.CharField(max_length=200)  # Le titre de la vidéo
+    file = models.FileField(upload_to='videos/')  # Le fichier vidéo
+    uploaded_at = models.DateTimeField(auto_now_add=True)  # La date d'upload
+
+    def __str__(self):
+        return self
